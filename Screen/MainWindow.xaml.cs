f240be2a5665fa91a0d5b72dcd62468eb187677e
Haqve_vu <novruzluqyds20@gmail.com>
2023-12-05 04:47:33 -0800

    Add project files.

---
+++ Screen/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Screen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket Client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        static IPAddress remote_ip = IPAddress.Parse("127.0.0.1");
+        static int remote_port = 4000;
+
+        EndPoint remote_endpoint = new IPEndPoint(remote_ip, remote_port);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+  
+
+            SetImageSource();
+        }
+
+        private async void SetImageSource()
+        {
+
+            while (true)
+            {
+                string message = "Send Screenshoot";
+
+                byte[] buffer = Encoding.Default.GetBytes(message);
+
+                Client.SendTo(buffer, remote_endpoint);
+
+
+                try
+                {
+                    // Use asynchronous methods for network operations
+                    var chunkSize = 1024; // Set your desired chunk size
+                    var receiveBuffer = new List<byte>();  // Gelen Byte-lar Burda Toplanir [1024 , 1024 , 640]
+                    int bytesRead; // Oxunan Byte-in olchusu bura yazilir 1024 , 450 , 230
+
+                    do
+                    {
+                        var chunk = new byte[chunkSize];
+                        bytesRead = await Task.Run(() =>
+                        {
+                            return Client.ReceiveFrom(chunk, SocketFlags.None, ref remote_endpoint);
+                        });
+
+                        if (bytesRead > 0)
+                        {
+
+                            receiveBuffer.AddRange(chunk.Take(bytesRead));
+                        }
+
+                    } while (bytesRead == chunkSize);
+
+                    var image = LoadImage(receiveBuffer.ToArray());
+
+                    // Use Dispatcher.InvokeAsync for UI updates
+                    await Dispatcher.InvokeAsync(() => { ImageFrame.Source = image; });
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions more specifically or log them
+                    MessageBox.Show($"Error loading image: {ex.Message}");
+                }
+
+            }
+        }
+
+
+
+        private static BitmapImage? LoadImage(byte[] imageData)
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = new MemoryStream(imageData);
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.EndInit();
+            return image;
+        }
+
+
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+#pragma warning disable
+
+namespace Server
+{
+    public class Server
+    {
+
+        static void Main(string[] args)
+        {
+            var Listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp
+            );
+
+
+            var IP = IPAddress.Parse("127.0.0.1");
+            var Port = 4000;
+
+            var EndPoint = new IPEndPoint(IP, Port);
+
+            var msg = "";
+            var len = 0;
+
+            Listener.Bind(EndPoint);  // Server 127.0.0.1:4000 EndPoint-ne Bind() Funksiyasi Ile Qosulur
+
+            EndPoint remote_EndPoint = new IPEndPoint(IPAddress.Any, 0);  // Remote EndPoint Server-le Elaqe Quracaq Client-in EndPoint-dir
+                                                                          // IPAddress.Any -> Istenilen IP addressin , 0 -> Istenilen Portu
+            while (true)
+            {
+                var buffer = new byte[ushort.MaxValue - 29];
+                len = Listener.ReceiveFrom(buffer, ref remote_EndPoint);
+
+                msg = Encoding.Default.GetString(buffer, 0, len);
+
+                if (msg == "Send Screenshoot")
+                {
+                    Console.WriteLine($"{remote_EndPoint.ToString()}: {msg}");
+
+                    var bitmap_image = CaptureMyScreen();
+
+                    var bytes_image = ImageToByte(bitmap_image);
+
+                    Console.WriteLine(bytes_image.Length);
+
+                    // Fragmentation
+                    int chunkSize = 1024; // Set your desired chunk size
+                    for (int i = 0; i < bytes_image.Length; i += chunkSize)
+                    {
+                        int remainingBytes = Math.Min(chunkSize, bytes_image.Length - i); // 1024 chixir her defe ta ki , alinan reqem 1024-den kicik olsun
+                        byte[] chunk = new byte[remainingBytes]; // chixilan olchude byte massivi yaradilir .
+                        Buffer.BlockCopy(bytes_image, i, chunk, 0, remainingBytes); // Ve Boyuk Image-den 1024 ve ya daha kick byte yaradilan massive kocurulur
+
+                        // Send each chunk
+                        try
+                        {
+                            Listener.SendTo(chunk, SocketFlags.None, remote_EndPoint);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error sending data: {ex.Message}");
+                            // Handle the error as needed
+                        }
+                        Console.WriteLine(chunk.Length);
+
+                    }
+
+
+                }
+            }
+
+        }
+
+        static private Bitmap CaptureMyScreen()
+        {
+            try
+            {
+                var width = Screen.PrimaryScreen.Bounds.Width;
+                var height = Screen.PrimaryScreen.Bounds.Height;
+                Bitmap Capture_bitmap = new Bitmap(width, height);
+
+                Rectangle Capture_rectangle = Screen.AllScreens[0].Bounds;
+
+                Graphics Capture_graphics = Graphics.FromImage(Capture_bitmap);
+                Capture_graphics.CopyFromScreen(Capture_rectangle.Left, Capture_rectangle.Top, 0, 0, Capture_rectangle.Size);
+
+                return Capture_bitmap;
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        static byte[] ImageToByte(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+    }
+}
